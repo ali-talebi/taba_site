f12db7b9a20d4816a327ae6013418f81a740ed8b
ali-talebi <alitalebishahroodi@gmail.com>
2025-06-02 20:10:44 +0330

    update level and fields school

--- core/school/admin.py
+++ core/school/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
-from .models import SchoolInformation , Level_Education_School
+from .models import SchoolInformation , Level_Education_School , Field_Education_School
 # Register your models here.
+
+
+@admin.register(Field_Education_School)
+class Field_Education_School_Admin(admin.ModelAdmin):
+    list_display = ('field_name' , ) 
+    
+    
 
 
 @admin.register(Level_Education_School)
 class Level_Education_School_Admin(admin.ModelAdmin):
-    list_display = ('name_level' , 'school' ) 
+    list_display = ('name_level' , 'school' ,'Total_Fields') 
     list_filter  = ('name_level' , ) 
     
     
+    def Total_Fields(self,obj):
+        total_fields = ' | '.join([ i.field_name for i in obj.fields.all() ])
+        return total_fields 
+    Total_Fields.short_description = " رشته های تحصیلی مدرسه "
 
 @admin.register(SchoolInformation)
 class SchoolInformation_Admin(admin.ModelAdmin):
--- core/school/models.py
+++ core/school/models.py
@@ -4,12 +4,26 @@ from MainAddress.models import Province
 # Create your models here.
 
 
+class Field_Education_School(models.Model) : 
+    field_name = models.CharField(verbose_name="نام رشته تحصیلی",max_length=30)
+
+    def __str__(self):
+        return self.field_name 
+    
+    class Meta : 
+        db_table = 'Field_Education_School'
+        verbose_name_plural = 'رشته های تحصیلی مدرسه '
+
 class Level_Education_School(models.Model):
     school = models.OneToOneField('SchoolInformation' , verbose_name='مدرسه' , on_delete=models.CASCADE , null = True )
     name_level = models.CharField(max_length=100 , verbose_name="سطح تحصیلی مدارس" )
+    fields     = models.ManyToManyField(Field_Education_School,verbose_name='رشته های تحصیلی') 
     
     def __str__(self):
         return self.name_level 
+    
+    
+
     
     class Meta : 
         db_table = "Level_Education_School"
@@ -22,6 +36,8 @@ class SchoolInformation(models.Model):
     logo        = models.FileField(verbose_name="لوگوی مدرسه" , null = True , blank = True , upload_to="SchoolInformation")
     manager     = models.OneToOneField(ManagerInformation , verbose_name="مدیر مدرسه" , on_delete=models.SET_NULL , null = True )
     address     = models.ForeignKey(Province , verbose_name='آدرس' , on_delete=models.SET_NULL , null = True ) 
+    
+    
     
     def __str__(self) : 
         return f'{self.school_name} - {self.address} '
